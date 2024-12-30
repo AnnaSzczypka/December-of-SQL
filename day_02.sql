a399438d7ee9c704dc8ea1357cc9eac5c531a7af
Anna Szczypka-Kuc <167246862+AnnaSzczypka@users.noreply.github.com>
2024-12-30 20:51:26 +0100

    Update day_02.sql

--- day_02.sql
+++ day_02.sql
@@ -1,4 +1,41 @@
--- Santa wants to know which gifts weigh more than 1 kg. Can you list them?
-select *
-from gifts 
-where weight_kg>1
+-------------------------------------------------------
+-- day 2: Identifying Heavy Gifts --
+-------------------------------------------------------
+
+-- Author: Anna Szczypka Kuc
+-- Tool used: MySQL
+
+-- https://www.sqlcalendar.com/app/sql-challenge/2
+
+/*
+Today's Question:
+
+Santa wants to know which gifts weigh more than 1 kg. 
+Can you list them?
+*/
+
+-- Table
+--      gifts
+
+-- Filtering
+
+-- Solution:
+SELECT 
+    * 
+FROM 
+    gifts 
+WHERE 
+    weight_kg > 1;
+
+/*
+SQL Functions and Concepts Used:
+
+1. **Filtering with WHERE:**
+   - Filters the gifts to include only those with a weight greater than 1 kg (`weight_kg > 1`).
+
+2. **Purpose:**
+   - Helps Santa identify which gifts are heavier and may need special attention for sleigh packing.
+
+3. **Output:**
+   - Returns all columns (`*`) for gifts weighing more than 1 kg.
+*/
