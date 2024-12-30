e0fd3341e39b36fb6ff65cceba0a2278efcf2120
Anna Szczypka-Kuc <167246862+AnnaSzczypka@users.noreply.github.com>
2024-12-30 21:48:38 +0100

    Update day_01_dataset.sql

--- dateset/day_01_dataset.sql
+++ dateset/day_01_dataset.sql
@@ -9,10 +9,10 @@ CREATE TABLE rentals (
 -- Insert data into the rentals table
 INSERT INTO rentals (rental_id, customer_name, activity, rental_date)
 VALUES
-    (1, 'Emily', 'Skiing', '2024-01-01'), -- Emily rented equipment for Skiing
-    (2, 'Michael', 'Snowboarding', '2024-01-02'), -- Michael rented equipment for Snowboarding
-    (3, 'Emily', 'Snowboarding', '2024-01-03'), -- Emily rented equipment for Snowboarding
-    (4, 'Sarah', 'Skiing', '2024-01-01'), -- Sarah rented equipment for Skiing
-    (5, 'Michael', 'Skiing', '2024-01-02'), -- Michael rented equipment for Skiing
-    (6, 'Michael', 'Snowtubing', '2024-01-02'); -- Michael rented equipment for Snowtubing
+    (1, 'Emily', 'Skiing', '2024-01-01'), 
+    (2, 'Michael', 'Snowboarding', '2024-01-02'), 
+    (3, 'Emily', 'Snowboarding', '2024-01-03'),
+    (4, 'Sarah', 'Skiing', '2024-01-01'),
+    (5, 'Michael', 'Skiing', '2024-01-02'),
+    (6, 'Michael', 'Snowtubing', '2024-01-02'); 
 
